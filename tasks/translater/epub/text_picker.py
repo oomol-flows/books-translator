ec45c769f1fa2e4a4d6ae5469738a8feacba05a6
TaoZeyu <i@taozeyu.com>
2024-09-23 12:06:20 +0800

    feat: support translate all text in body of html

---
+++ tasks/translater/epub/text_picker.py
@@ -0,0 +1,165 @@
+from __future__ import annotations
+from typing import Any, Union, Optional, Literal
+from lxml.etree import tostring, Element
+
+_TEXT_TAG = (
+  "h1", "h2", "h3", "h4", "h5", "h6",
+  "p", "span", "em", "strong", "blockquote", 
+  "pre", "code", "hr", "label",
+)
+_BAN_TO_STRING_TAG = (
+  "style", "css", "script", "metadata"
+)
+
+class _BaseDom:
+  def __init__(self, dom, children: list[WrappedDom] = []):
+    self.dom = dom
+    self.children = children
+    self._should_child_dom_to_string: Optional[bool] = None
+
+  def to_string(self, method: Literal["html", "text"]) -> str:
+    bin = tostring(self.dom, method=method, encoding="utf-8", pretty_print=False)
+    return bin.decode("utf-8")
+
+  @property
+  def should_child_dom_to_string(self) -> bool:
+    if self._should_child_dom_to_string is None:
+      self._should_child_dom_to_string = self._check_should_child_dom_to_string()
+    return self._should_child_dom_to_string
+
+  def _check_should_child_dom_to_string(self) -> bool:
+    if isinstance(self, TextDom):
+      return True
+    else:
+      has_child = False
+      for child in self.children:
+        has_child = True
+        if isinstance(child, TreeDom):
+          return False
+      return has_child
+
+class TreeDom(_BaseDom):
+  pass
+
+class TextDom(_BaseDom):
+  pass
+
+WrappedDom = Union[str, _BaseDom]
+
+class TextPicker:
+  def __init__(self, root, method: Literal["html", "text"]):
+    self._root = root
+    self._method: Literal["html", "text"] = method
+    self._wrapped_root: Optional[_BaseDom] = None
+
+  def pick_texts(self) -> list[str]:
+    texts: list[str] = []
+    self._wrapped_root = self._wrap_dom(self._root)
+    self._collect_texts(self._wrapped_root, texts)
+    return texts
+
+  def append_texts(self, texts: list[Optional[str]]):
+    if self._wrapped_root is None:
+      raise Exception("should call pick_texts before")
+    
+    texts = texts.copy()
+    texts.reverse() # to pop text from last to first
+    self._append_texts_after_dom(texts, self._wrapped_root)
+
+  def to_string(self) -> str:
+    bin = tostring(self._root, method="html", encoding="utf-8", pretty_print=False)
+    return bin.decode("utf-8")
+
+  def _wrap_dom(self, dom) -> _BaseDom:
+    children: list[WrappedDom] = []
+    has_tree_children: bool = False
+
+    if self._is_not_empty_str(dom.text):
+      children.append(dom.text)
+
+    for child in dom.getchildren():
+      wraped_child = self._wrap_dom(child)
+      children.append(wraped_child)
+      if isinstance(wraped_child, TreeDom):
+        has_tree_children = True
+      if self._is_not_empty_str(child.tail):
+        children.append(child.tail)
+
+    if not has_tree_children and \
+       self._is_text_tag(dom.tag):
+      return TextDom(dom, children)
+    else:
+      return TreeDom(dom, children)
+
+  def _collect_texts(self, wrapped_dom: _BaseDom, texts: list[str]):
+    if self._is_ban_to_string_tag(wrapped_dom.dom.tag):
+      return
+    if wrapped_dom.should_child_dom_to_string:
+      texts.append(wrapped_dom.to_string(self._method))
+    else:
+      for child in wrapped_dom.children:
+        if isinstance(child, str):
+          texts.append(child)
+        else:
+          self._collect_texts(child, texts)
+
+  def _append_texts_after_dom(self, texts: list[Optional[str]], wrapped_dom: _BaseDom) -> Optional[Any]:
+    if self._is_ban_to_string_tag(wrapped_dom.dom.tag):
+      return None
+
+    if wrapped_dom.should_child_dom_to_string:
+      text = texts.pop()
+      if text is None:
+        return None
+      return self._append_text_after_dom(wrapped_dom.dom, text)
+
+    else:
+      last_child = None
+      for child in wrapped_dom.children:
+        if isinstance(child, str):
+          text = texts.pop()
+          if text is None:
+            text = child
+          else:
+            text = f"{child}\n{text}"
+          if last_child is None:
+            wrapped_dom.dom.text = text
+          else:
+            last_child.tail = text
+        else:
+          append_dom = self._append_texts_after_dom(texts, child)
+          if append_dom is not None:
+            last_child = append_dom
+
+      return wrapped_dom.dom
+
+  def _append_text_after_dom(self, dom, text: str) -> Optional[Any]:
+    new_dom = Element(dom.tag)
+    new_dom.text = text
+    for key, value in dom.attrib.items():
+      new_dom.attrib[key] = value
+
+    parent = dom.getparent()
+    if parent is None:
+      return None # cannot do anything
+
+    index = parent.getchildren().index(dom)
+    parent.insert(index+1, new_dom)
+    dom.tail = None
+    return new_dom
+
+  def _is_text_tag(self, tag: str) -> bool:
+    global _TEXT_TAG
+    return tag in _TEXT_TAG
+
+  def _is_ban_to_string_tag(self, tag: str) -> bool:
+    global _BAN_TO_STRING_TAG
+    return tag in _BAN_TO_STRING_TAG
+
+  def _is_not_empty_str(self, text: Optional[str]) -> bool:
+    if text is None:
+      return False
+    for char in text:
+      if char != " " and char != "\n":
+          return True
+    return False
---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+
+try:
+  loader = unittest.TestLoader()
+  suite = loader.discover("tasks/translater/tests")
+  runner = unittest.TextTestRunner()
+  result = runner.run(suite)
+  if not result.wasSuccessful():
+    exit(1)
+except Exception as e:
+  print(e)
+  exit(1)
