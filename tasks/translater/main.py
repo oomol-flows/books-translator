1b6cd38c74df295cf49315610cafc1489430251d
Moskize91 <i@taozeyu.com>
2025-01-03 10:52:35 +0800

    feat(block): support new context LLM env & support streaming mode & ja lan (#14)

--- tasks/translater/main.py
+++ tasks/translater/main.py
@@ -2,34 +2,25 @@ from oocana import Context
 from shared.epub import EpubHandler
 from shared.translater import Translater, LLM_API
 from .file import translate_epub_file
+from .llm_parser import parse
 
-def main(inputs: dict, context: Context):
-  llm_api: str = inputs["llm_api"]
-  api: LLM_API
-
-  if llm_api == "openai":
-    api = LLM_API.OpenAI
-  elif llm_api == "claude":
-    api = LLM_API.Claude
-  elif llm_api == "gemini":
-    api = LLM_API.Gemini
-  else:
-    raise Exception(f"unknown llm_api: {llm_api}")
-
-  timeout: float | None = inputs["timeout"]
+def main(params: dict, context: Context):
+  llm = parse(params, context)
+  timeout: float | None = params["timeout"]
   if timeout == 0.0:
     timeout = None
 
   translater = Translater(
-    api=api,
-    key=_none_if_empty(inputs["api_key"]),
-    url=_none_if_empty(inputs["url"]),
-    model=inputs["model"],
-    temperature=inputs["temperature"],
+    api=llm.api,
+    key=llm.key,
+    url=llm.url,
+    model=llm.model,
+    temperature=params["temperature"],
     timeout=timeout,
-    source_lan=inputs["source"],
-    target_lan=inputs["target"],
-    group_max_tokens=inputs["max_translating_group"],
+    source_lan=params["source"],
+    target_lan=params["target"],
+    group_max_tokens=params["max_translating_group"],
+    streaming=True,
   )
   epub_handler = EpubHandler(
     translate=translater.translate,
@@ -37,10 +28,7 @@ def main(inputs: dict, context: Context):
   zip_data = translate_epub_file(
     context=context,
     handler=epub_handler,
-    file_path=inputs["file"],
-    book_title=inputs.get("title", None),
+    file_path=params["file"],
+    book_title=params.get("title", None),
   )
   return { "binary": zip_data }
-
-def _none_if_empty(text: str) -> str | None:
-  return text.strip() == "" if None else text
