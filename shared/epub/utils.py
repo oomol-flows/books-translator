0c13eea1450d5992983ac6f59ec67d0bf66f38fc
Moskize91 <i@taozeyu.com>
2025-03-16 11:00:55 +0800

    chore(project): release 0.0.7 (#18)

--- shared/epub/utils.py
+++ shared/epub/utils.py
@@ -9,7 +9,7 @@ def create_node(node_str: str, parser):
 def escape_ascii(content: str) -> str:
   content = escape(content)
   content = re.sub(
-    r"\\u([\da-fA-F]{4})", 
+    r"\\u([\da-fA-F]{4})",
     lambda x: chr(int(x.group(1), 16)), content,
   )
   return content
